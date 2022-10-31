965311f625b81c1dc5cd3dce4742f78526eefd46
Benjamin Croad <benjamincroadapple@gmail.com>
2022-10-31 11:23:51 +0000

    Added call to top-6-recipes api

--- client/src/components/recipes/recipeCard.jsx
+++ client/src/components/recipes/recipeCard.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import recipeCardStyles from './recipeCard.module.css'
 
-export default class RecipeCard extends Component {
-  render() {
-    return (
-      <div class={recipeCardStyles.card}>
-        <p>Breakfast</p>
-      </div>
-    )
-  }
+function RecipeCard(props) {
+  return (
+    <div className={recipeCardStyles.card}>
+      <p className={recipeCardStyles.cardText}>{props.title}</p>
+    </div>
+  )
 }
+
+export default RecipeCard;
--- client/src/components/recipes/recipeList.jsx
+++ client/src/components/recipes/recipeList.jsx
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import RecipeCard from './recipeCard';
 import recipeListStyles from './recipeList.module.css'
 
-export default class RecipeList extends Component {
-  render() {
-    return (
-      <div class={recipeListStyles.cardList}>
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-      </div>
-    )
-  }
+function RecipeList() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("http://localhost:3001/api/v1/recipes/top-6-recipes")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.data.data);
+        setData(data.data.data);
+      });
+  }, []);
+
+  return (
+    <div className={recipeListStyles.cardList}>
+      {data.map(({ _id, title }) => <RecipeCard id={ _id } key={ _id } title={ title } />)}
+      {/* <RecipeCard name={!data ? "Loading..." : data[1].title} /> */}
+    </div>
+  )
 }
+
+export default RecipeList;
